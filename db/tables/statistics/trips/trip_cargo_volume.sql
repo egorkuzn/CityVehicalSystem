3af62ac8ff98b3e1caacf3dd03149755018243e9
shift-lab-kuznetsov <e.kuznetsov1@g.nsu.ru>
2023-03-10 23:16:15 +0700

    Primary key on sub trip tables

--- db/tables/statistics/trips/trip_cargo_volume.sql
+++ db/tables/statistics/trips/trip_cargo_volume.sql
@@ -1,5 +1,5 @@
 create table "Trip-Cargo volume" (
-    trip_id      int not null check ( trip_id > 0 ),
+    trip_id      int primary key check ( trip_id > 0 ),
     cargo_volume smallint not null check ( cargo_volume > 0),
 
     foreign key (cargo_volume) references "Trips"
--- db/tables/statistics/trips/trip_passengers_volume.sql
+++ db/tables/statistics/trips/trip_passengers_volume.sql
@@ -1,5 +1,5 @@
 create table "Trip-Passengers volume" (
-    trip_id      int not null check ( trip_id > 0 ),
+    trip_id      int primary key check ( trip_id > 0 ),
     passengers_volume smallint not null check ( passengers_volume > 0),
 
     foreign key (passengers_volume) references "Trips"
