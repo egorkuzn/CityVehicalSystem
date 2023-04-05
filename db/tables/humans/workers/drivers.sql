89727c6e268acff8c0a897b20f855c850a93afd2
shift-lab-kuznetsov <e.kuznetsov1@g.nsu.ru>
2023-04-05 13:48:18 +0700

    Queries initialisation

--- db/tables/humans/workers/drivers.sql
+++ db/tables/humans/workers/drivers.sql
@@ -1,5 +1,15 @@
 create table "Drivers" (
-    worker_id int primary key check ( worker_id > 0 ),
+                           worker_id int primary key check ( worker_id > 0 ),
 
-    foreign key (worker_id) references "Workers"
+                           foreign key (worker_id) references "Workers"
+);
+
+create table "Drivers-Vehicles" (
+    driver_id int not null check ( driver_id > 0 ),
+    vehicle_id int not null check (vehicle_id > 0),
+
+    primary key (driver_id, vehicle_id),
+
+    foreign key (driver_id) references "Drivers",
+    foreign key (vehicle_id) references "Vehicle"
 );
--- db/tables/vehicles/vehicles.sql
+++ db/tables/vehicles/vehicles.sql
@@ -1,7 +1,6 @@
 create table "Vehicle" (
     vehicle_id serial primary key,
     garage_id  int not null check ( garage_id > 0 ),
-    driver_id  int not null check ( driver_id > 0 ),
     year       smallint not null check ( year <= date_part('year', CURRENT_DATE) AND year >= 1930 ),
     -- True is active
     -- False is archived
@@ -13,6 +12,5 @@ create table "Vehicle" (
                                     AND (archive_year <= date_part('year', CURRENT_DATE) AND archive_year >= 1930)
                                     AND (add_year <= archive_year) OR status) DEFAULT -1,
 
-    foreign key (garage_id) references "Garages",
-    foreign key (driver_id) references "Drivers"
+    foreign key (garage_id) references "Garages"
 );
